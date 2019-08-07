08e129f1d86dd107c16c00c839ddc7765f6fd641
TaJ <sifat.taj@gmail.com>
2019-08-07 18:59:30 +0600

    Client Fix

--- src/tcp_version/MemeClient.java
+++ src/tcp_version/MemeClient.java
@@ -1,61 +1,33 @@
 package tcp_version;
 
-import java.io.BufferedReader;
 import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.InputStreamReader;
 import java.net.Socket;
-import java.util.Scanner;
 
-class Client{
-    public static void main(String args[])throws Exception{
-        String address = "";
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter Server Address: ");
-        address=sc.nextLine();
+class Client {
+    public static void main(String args[]) throws Exception {
+        String address = "localhost";
 
-        Socket s=new Socket(address,5000);
-        DataInputStream din=new DataInputStream(s.getInputStream());
-        DataOutputStream dout=new DataOutputStream(s.getOutputStream());
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        Socket s = new Socket(address, 5000);
+        DataInputStream din = new DataInputStream(s.getInputStream());
 
-        System.out.println("Send Get to start...");
-        String str="",filename="";
-        try{
-            while(!str.equals("start"))
-                str=br.readLine();
-
-            dout.writeUTF(str);
-            dout.flush();
-
-            filename=din.readUTF();
-            System.out.println("Receving file: "+filename);
-            filename="client"+filename;
-            System.out.println("Saving as file: "+filename);
-//
-            long sz=Long.parseLong(din.readUTF());
-            System.out.println ("File Size: "+ (sz) +" Byte");
-
-            byte b[]=new byte [1024];
-            System.out.println("Receving file..");
-            FileOutputStream fos=new FileOutputStream(new File(filename),true);
+        String filename = "data_from_tcp.rtf";
+        try {
+            byte b[] = new byte[1024];
+            FileOutputStream fos = new FileOutputStream(new File(filename), true);
             long bytesRead;
-            do
-            {
+            do {
                 bytesRead = din.read(b, 0, b.length);
-                fos.write(b,0,b.length);
-            }while(!(bytesRead<1024));
-            System.out.println("Comleted");
+                fos.write(b, 0, b.length);
+            } while (!(bytesRead < 1024));
+            System.out.println("Received");
             fos.close();
-            dout.close();
+
             s.close();
-        }
-        catch(EOFException e)
-        {
-            //do nothing
+        } catch (EOFException e) {
+            e.printStackTrace();
         }
     }
 }
