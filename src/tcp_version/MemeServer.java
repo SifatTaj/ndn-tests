6188c2e82a40b228a2f99c38c37aa5ada78e17e4
TaJ <sifat.taj@gmail.com>
2019-08-08 00:51:07 +0600

    Auto Discover Server

--- src/tcp_version/MemeServer.java
+++ src/tcp_version/MemeServer.java
@@ -1,15 +1,14 @@
 package tcp_version;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 import java.util.Scanner;
 
 class Server {
     public static void main(String args[]) throws Exception {
+
+        Thread discoveryThread = new Thread(DiscoveryThread.getInstance());
+        discoveryThread.start();
 
         String filename = "lorem.rtf";
 
@@ -52,4 +51,55 @@ class Server {
             ss.close();
         }
     }
+}
+
+class DiscoveryThread implements Runnable {
+
+    DatagramSocket socket;
+
+    @Override
+    public void run() {
+        try {
+            //Keep a socket open to listen to all the UDP trafic that is destined for this port
+            socket = new DatagramSocket(8888, InetAddress.getByName("0.0.0.0"));
+            socket.setBroadcast(true);
+
+            while (true) {
+                System.out.println(getClass().getName() + ">>>Ready to receive broadcast packets!");
+
+                //Receive a packet
+                byte[] recvBuf = new byte[15000];
+                DatagramPacket packet = new DatagramPacket(recvBuf, recvBuf.length);
+                socket.receive(packet);
+
+                //Packet received
+                System.out.println(getClass().getName() + ">>>Discovery packet received from: " + packet.getAddress().getHostAddress());
+                System.out.println(getClass().getName() + ">>>Packet received; data: " + new String(packet.getData()));
+
+                //See if the packet holds the right command (message)
+                String message = new String(packet.getData()).trim();
+                if (message.equals("DISCOVER_FUIFSERVER_REQUEST")) {
+                    byte[] sendData = "DISCOVER_FUIFSERVER_RESPONSE".getBytes();
+
+                    //Send a response
+                    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, packet.getAddress(), packet.getPort());
+                    socket.send(sendPacket);
+
+                    System.out.println(getClass().getName() + ">>>Sent packet to: " + sendPacket.getAddress().getHostAddress());
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static DiscoveryThread getInstance() {
+        return DiscoveryThreadHolder.INSTANCE;
+    }
+
+    private static class DiscoveryThreadHolder {
+
+        private static final DiscoveryThread INSTANCE = new DiscoveryThread();
+    }
+
 }
